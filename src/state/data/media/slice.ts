e55e89da05d236df07b362a3ab67a1f8690091f6
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-11-21 03:49:28 +0700

    Add media slice

---
+++ src/state/data/media/slice.ts
@@ -0,0 +1,102 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { commonAsyncState, commonState } from "constants/common/state";
+import { generateCommonAsyncReducer } from "helpers/state";
+import { MediaFile, MediaCriteria, MediaState } from "types/media";
+
+export const initialState: MediaState = {
+  ...commonState,
+  data: {
+    criteria: {
+      count: 20,
+      page: 0,
+    },
+    hasMore: true,
+    result: null,
+  },
+  status: {
+    add: commonAsyncState,
+    get: commonAsyncState,
+    remove: commonAsyncState,
+    update: commonAsyncState,
+  },
+};
+
+const asyncReducer = {
+  add: generateCommonAsyncReducer<MediaState, MediaFile>(initialState, "add"),
+  get: generateCommonAsyncReducer<
+    MediaState,
+    Partial<MediaCriteria> | undefined
+  >(initialState, "get"),
+  remove: generateCommonAsyncReducer<MediaState, MediaFile>(
+    initialState,
+    "remove"
+  ),
+  update: generateCommonAsyncReducer<MediaState, MediaFile>(
+    initialState,
+    "update"
+  ),
+};
+
+const slice = createSlice({
+  initialState,
+  name: "data/media",
+  reducers: {
+    add: asyncReducer.add.default,
+    addFailed: asyncReducer.add.failed,
+    addPending: asyncReducer.add.pending,
+    addSucceeded: asyncReducer.add.succeeded,
+
+    clearResult: (state) => ({
+      data: {
+        ...initialState.data,
+        criteria: {
+          ...state.data.criteria,
+          page: 0,
+        },
+        hasMore: true,
+      },
+      status: { ...initialState.status },
+    }),
+
+    get: asyncReducer.get.default,
+    getFailed: asyncReducer.get.failed,
+    getPending: asyncReducer.get.pending,
+    getSucceeded: asyncReducer.get.succeeded,
+
+    remove: asyncReducer.remove.default,
+    removeFailed: asyncReducer.remove.failed,
+    removePending: asyncReducer.remove.pending,
+    removeSucceeded: asyncReducer.remove.succeeded,
+
+    reset: () => ({ ...initialState }),
+
+    update: asyncReducer.update.default,
+    updateFailed: asyncReducer.update.failed,
+    updatePending: asyncReducer.update.pending,
+    updateSucceeded: asyncReducer.update.succeeded,
+  },
+});
+
+export const {
+  add,
+  addFailed,
+  addPending,
+  addSucceeded,
+  clearResult,
+  get,
+  getFailed,
+  getPending,
+  getSucceeded,
+  remove,
+  removeFailed,
+  removePending,
+  removeSucceeded,
+  reset,
+  update,
+  updateFailed,
+  updatePending,
+  updateSucceeded,
+} = slice.actions;
+
+export default slice.reducer;
