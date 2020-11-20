098dcca1709132f715eefc0a90f310c31a81141f
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-11-21 00:55:00 +0700

    Add Button component

---
+++ src/components/common/base/Button/index.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Button as bsButton } from "react-bootstrap";
+
+const Button = styled(bsButton)`
+  border-radius: 30px;
+  border: 1px solid #bdbdbd;
+`;
+
+export default Button;
