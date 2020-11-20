5f7a3e76459ce112fb104b16bfd2b721fe60c380
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-11-21 03:46:20 +0700

    Add criteria type definition

---
+++ src/types/criteria.ts
@@ -0,0 +1,14 @@
+export type Sort = {
+  direction: 'asc' | 'desc';
+  property: string;
+};
+
+export type Criteria = {
+  [key: string]: any;
+};
+
+export interface PaginateCriteria extends Criteria {
+  count: number;
+  page: number;
+  sort?: Sort;
+}
