b3c7f5a769df72ec1ae8518b9ab9d08c75096fd5
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-11-21 00:56:28 +0700

    Remove react hot loader from app component

--- src/components/core/skeleton/App/index.tsx
+++ src/components/core/skeleton/App/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-import { hot } from "react-hot-loader/root";
 
 import Body from "components/core/document/Body";
 import Wrapper from "components/core/layout/Wrapper";
@@ -13,4 +12,4 @@ const App: FC = () => (
   </Wrapper>
 );
 
-export default hot(App);
+export default App;
