472a119a25a96aabe7ae02c2e921a8b949cd10a3
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-11-21 03:46:50 +0700

    Add media type definition

---
+++ src/types/media.ts
@@ -0,0 +1,26 @@
+import { CommonAsyncState, CommonState } from 'types/state';
+import { PaginateCriteria } from './criteria';
+
+export interface MediaCriteria extends PaginateCriteria {
+    name?: string;
+}
+
+export type MediaFile = {
+    id: string;
+    // name: string;
+    // status: boolean;
+};
+
+export interface MediaState extends CommonState {
+    data: {
+        criteria: MediaCriteria;
+        hasMore: boolean;
+        result: MediaFile[] | null;
+    };
+    status: {
+        add: CommonAsyncState;
+        get: CommonAsyncState;
+        remove: CommonAsyncState;
+        update: CommonAsyncState;
+    };
+}
