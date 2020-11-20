6c75caac6750d5cd3488c21fdeaf4cd2fa89c854
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-11-21 03:48:02 +0700

    Add state constants

---
+++ src/constants/common/state.ts
@@ -0,0 +1,12 @@
+import { CommonAsyncState, CommonState } from 'types/state';
+
+export const commonAsyncState: CommonAsyncState = {
+  error: null,
+  loaded: false,
+  loading: false,
+};
+
+export const commonState: CommonState = {
+  data: null,
+  status: {},
+};
