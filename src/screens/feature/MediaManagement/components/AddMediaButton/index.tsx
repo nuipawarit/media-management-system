fee6316a65938a435b08c38c36319f1bab35314a
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-11-28 17:22:25 +0700

    Implement file upload functionality to add media button

--- src/screens/feature/MediaManagement/components/AddMediaButton/index.tsx
+++ src/screens/feature/MediaManagement/components/AddMediaButton/index.tsx
@@ -1,20 +1,44 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC, useRef } from "react";
 import { Fab } from "react-tiny-fab";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = {
-  onClick?: () => void;
+  onFileChange?: (files: FileList) => void;
 };
 
-const AddMediaButton: FC<Props> = ({ onClick }) => {
+const AddMediaButton: FC<Props> = ({ onFileChange = () => {} }) => {
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
+
+  const onClickHandler = () => {
+    if (!hiddenFileInput.current) return;
+
+    hiddenFileInput.current.click();
+  };
+
+  const onFileChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const fileUploaded = event.target.files;
+
+    if (fileUploaded !== null) onFileChange(fileUploaded);
+  };
+
   return (
-    <Fab
-      icon={<FontAwesomeIcon icon={["fas", "plus"]} />}
-      onClick={onClick}
-      style={{ bottom: 24, right: 18, zIndex: 1000 }}
-      text="Add new media"
-    />
+    <>
+      <Fab
+        icon={<FontAwesomeIcon icon={["fas", "plus"]} />}
+        onClick={onClickHandler}
+        style={{ bottom: 24, right: 18, zIndex: 1000 }}
+        text="Add new media"
+      />
+      <input
+        accept=".jpg,.jpeg,.png,.mp4"
+        multiple
+        ref={hiddenFileInput}
+        onChange={onFileChangeHandler}
+        style={{ display: "none" }}
+        type="file"
+      />
+    </>
   );
 };
 
