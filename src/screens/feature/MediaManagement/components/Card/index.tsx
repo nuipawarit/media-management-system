c9a84dfc70be082e316b0de0a812ed411a8f6738
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-12-03 00:12:25 +0700

    Add card hover effect

--- src/screens/feature/MediaManagement/components/Card/index.tsx
+++ src/screens/feature/MediaManagement/components/Card/index.tsx
@@ -10,7 +10,27 @@ import { shortDateFormat } from "helpers/datetime";
 import { MediaFile } from "types/media";
 
 const Box = styled(BsCard)`
+  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
+  position: relative;
+  translation: box-shadow 0.3s ease-in-out;
   width: calc(12rem + 2px);
+
+  ::before {
+    bottom: 0;
+    box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175) !important;
+    content: "";
+    left: 0;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transition: opacity 0.2s;
+  }
+
+  :hover::before {
+    cursor: pointer;
+    opacity: 1;
+  }
 `;
 
 const Image = styled(BsCard.Img)`
