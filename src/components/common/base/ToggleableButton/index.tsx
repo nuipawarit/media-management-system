511792e09fed4b60d3e18e2aec48c1da3e42c7ad
Nui Pawarit <ext.pawarit.cho@truedigital.com>
2020-12-04 02:51:15 +0700

    Implement toggle button click behavior

--- src/components/common/base/ToggleableButton/index.tsx
+++ src/components/common/base/ToggleableButton/index.tsx
@@ -1,16 +1,42 @@
 import React, { ComponentProps, FC } from "react";
 
+import styled from "styled-components";
+
 import Button from "../Button";
 
+const Box = styled(Button)`
+  &.active {
+    background-color: #939393 !important;
+    border-color: #939393 !important;
+    color: #f1f1f1 !important;
+  }
+  
+  &.active:hover {
+    background-color: #b6b6b6 !important;
+    border-color: #b6b6b6 !important;
+    color: #f1f1f1 !important;
+  }
+`;
+
 type Props = ComponentProps<typeof Button> & {
-  active: boolean;
+  active?: boolean;
+  name: string;
+  onClick?: (name: string, value: boolean) => void;
 };
 
-const ToggleableButton: FC<Props> = ({ children, ...props }) => {
+const ToggleableButton: FC<Props> = ({
+  active,
+  name,
+  children,
+  onClick = () => {},
+  ...props
+}) => {
+  const onClickHandler = () => onClick(name, !active);
+
   return (
-    <Button variant="light" {...props}>
+    <Box active={active} variant="light" onClick={onClickHandler} {...props}>
       {children}
-    </Button>
+    </Box>
   );
 };
 
